9710af614061c3e8ae2199b630de160ea3de4181
merhabareis <308869529+merhabareis@users.noreply.github.com>
2026-08-26 12:46:50 +0300

    Correction of a grammar mistake

--- QWKERNEL011.py
+++ QWKERNEL011.py
@@ -19,7 +19,7 @@ RESET = "\033[0m"
 CURRENT_COLOR = CYAN
 
 def boot_system():
-    print(f"{GREEN}QW Kernel [Version 0.0.8 Extended DOS Edition]")
+    print(f"{GREEN}QW Kernel Version 0.1.1")
     print("(C) Copyright merhabareis All rights reserved.\n")
     print("Booting kernel...")
     time.sleep(1)
